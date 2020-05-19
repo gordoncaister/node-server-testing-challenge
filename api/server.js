53e7e6744ef4900c701cc3421d110af5b21e4f22
Gordon Caister <gordoncaister@gmail.com>
2020-05-19 14:27:46 -0500

    server routes working

---
+++ api/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const server = express();
+
+const db = require("../data/dbConfig")
+server.use(express.json());
+
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+server.get("/api/pancakes",(req,res)=>{
+  db("pancakes").select("id","type")
+  .then(pancakes => {
+    console.log(pancakes)
+    res.status(200).json(pancakes)
+  })  
+})
+
+server.post("/api/pancakes", (req,res)=> {
+  db("pancakes")
+    .insert(req.body)
+    .then(pancake =>res.status(201).json(pancake))
+    .catch(e => res.status(500).json({error:"error"}))
+})
+
+server.delete("/api/pancakes/:id",(req,res)=> {
+  db("pancakes")
+  .where("id",req.params.id)
+  .del()
+  .then(p => 
+    res.status(200).json({message: "deleted"})
+  )
+  .catch(e => res.status(500).json({error:"error"}))
+})
+
+module.exports = server;
